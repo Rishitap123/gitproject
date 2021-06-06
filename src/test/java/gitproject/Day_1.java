64c3ce929b90250309bc6cbfa2d5aa22f1df5189
rishitaprakash1999@gmail.com <rishitaprakash1999@gmail.com>
2021-06-06 18:49:19 +0530

    create gitproject

---
+++ src/test/java/gitproject/Day_1.java
@@ -0,0 +1,20 @@
+package gitproject;
+
+public class Day_1 {
+	static void pattern(int n){
+        for (int i=0;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        pattern(4);
+    }
+}
+		
+
+
