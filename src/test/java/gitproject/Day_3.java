12888a5cc9d40ccfc2abc47e18cb19fa9e2db577
rishitaprakash1999@gmail.com <rishitaprakash1999@gmail.com>
2021-06-07 08:18:32 +0530

    Day_3

---
+++ src/test/java/gitproject/Day_3.java
@@ -0,0 +1,18 @@
+package gitproject;
+
+import java.util.Scanner;
+public class Day_3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter three number");
+		int a = s.nextInt();
+		int b = s.nextInt();
+		int c = s.nextInt();
+		int sum = (a+b+c);
+		System.out.println("sum of three number is:" +sum);
+		
+
+	}
+
+}
